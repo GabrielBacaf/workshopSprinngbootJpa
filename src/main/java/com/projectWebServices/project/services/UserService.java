9972c1835f9014027ab18eebc5fd1229a2443b71
Gabriel Baca <gabryelbaca1@gmail.com>
2024-11-13 17:22:43 -0400

    error handling of FindById returning null

--- src/main/java/com/projectWebServices/project/services/UserService.java
+++ src/main/java/com/projectWebServices/project/services/UserService.java
@@ -2,6 +2,7 @@ package com.projectWebServices.project.services;
 
 import com.projectWebServices.project.entities.User;
 import com.projectWebServices.project.repositories.UserRepository;
+import com.projectWebServices.project.services.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 
     }
 
---
+++ src/main/java/com/projectWebServices/project/services/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.projectWebServices.project.services.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(Object id){
+        super("Resource not found. Id" + id);
+    }
+}
